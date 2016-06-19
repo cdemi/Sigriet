1aa01874b9bc27c00ae3de9b4a6fbff8a19b5f55
Christopher Demicoli <christopher.demicoli@outlook.com>
2016-06-19 15:50:57 +0200

    Testing some stuff

---
+++ Sigriet/Controllers/RecordingController.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Http;
+
+namespace Sigriet.Controllers
+{
+    public class RecordingController : ApiController
+    {
+        public void Add()
+        {
+            var file = HttpContext.Current.Request.Files.Count > 0 ?
+                HttpContext.Current.Request.Files[0] : null;
+
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+
+                var path = Path.Combine(
+                    HttpContext.Current.Server.MapPath("~/"),
+                    fileName
+                );
+
+                file.SaveAs(path);
+            }
+        }
+    }
+}
